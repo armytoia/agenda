19823b2f6e69967621ccded8123552c889b80b89
armytoia <vitoriaacaldeirinha@gmail.com>
2019-08-12 12:06:43 -0300

    sexta_versao

--- src/app/cidade/listar-cidade/listar-cidade.page.ts
+++ src/app/cidade/listar-cidade/listar-cidade.page.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { Cidade } from '../entidade/cidade';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-listar-cidade',
   templateUrl: './listar-cidade.page.html',
   styleUrls: ['./listar-cidade.page.scss'],
 })
 export class ListarCidadePage implements OnInit {
+  listaCidade: Observable<Cidade[]>;
 
-  constructor() { }
-
+    constructor(private fire: AngularFireDatabase) {
+      this.listaCidade = this.fire.list<Cidade>('cidade').snapshotChanges().pipe(
+        map( lista => lista.map(linha => ({ key: linha.payload.key, ... linha.payload.val() })))
+      );
+    }
   ngOnInit() {
   }
 
